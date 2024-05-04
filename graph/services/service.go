de307dc49f0a3837874871386ab2e499c184ca7f
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-05-04 15:43:08 +0900

    feat: add interface

--- graph/services/service.go
+++ graph/services/service.go
@@ -3,8 +3,25 @@ package services
 import (
 	"context"
 	"github.com/ogiogidayo/GitHub-API/graph/model"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
+
+func New(exec boil.ContextExecutor) Services {
+	return &services{
+		userService: &userService{exec: exec},
+	}
+}
 
 type UserService interface {
 	GetUserByName(ctx context.Context, name string) (*model.User, error)
+}
+
+type Services interface {
+	UserService
+	// issueテーブルを扱うIssueServiceなど、他のサービスインターフェースができたらそれらを追加していく
+}
+
+type services struct {
+	*userService
+	// issueテーブルを扱うissueServiceなど、他のサービス構造体ができたらフィールドを追加していく
 }
